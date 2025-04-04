8f3fea11d0084e25c8bb92e7dcb39d94b9851e6b
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-04 17:30:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
 import RegisterPage from "./pages/RegisterPage"
+import FindJobPage from "./pages/FindJobPage"
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/signin" element={<SignInPage />}/>
                 <Route path="/register" element={<RegisterPage />}/>
+                <Route path="/find-job" element={<FindJobPage />}/>
             </Routes>
         </BrowserRouter>
     )
