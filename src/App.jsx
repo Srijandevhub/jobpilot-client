e5b8899fc5bb6fc615a4c8174b3304809ff26d09
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-08 15:33:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
 import RegisterPage from "./pages/RegisterPage"
 import FindJobPage from "./pages/FindJobPage"
+import JobDetailsPage from "./pages/JobDetailsPage"
+import OverviewPage from "./pages/OverviewPage"
+import PostJobPage from "./pages/PostJobPage"
 
 const App = () => {
     return (
@@ -12,6 +15,9 @@ const App = () => {
                 <Route path="/signin" element={<SignInPage />}/>
                 <Route path="/register" element={<RegisterPage />}/>
                 <Route path="/find-job" element={<FindJobPage />}/>
+                <Route path="/job-details" element={<JobDetailsPage />}/>
+                <Route path="/dashboard/overview" element={<OverviewPage />}/>
+                <Route path="/dashboard/postjob" element={<PostJobPage />}/>
             </Routes>
         </BrowserRouter>
     )
