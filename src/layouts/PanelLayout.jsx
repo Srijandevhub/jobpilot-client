c684126366b89a8c6f08d89895322714aed4dae0
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-17 12:24:05 +0530

    Update PanelLayout.jsx

--- src/layouts/PanelLayout.jsx
+++ src/layouts/PanelLayout.jsx
@@ -1,19 +1,35 @@
 import { Col, Container, Row } from "react-bootstrap"
 import MainHeader from "../components/MainHeader/MainHeader"
 import PanelSidebar from "../components/PanelSidebar/PanelSidebar"
+import { useEffect, useState } from "react"
+import PanelSidebarMobile from "../components/PanelSidebar/PanelSidebarMobile"
 
 const PanelLayout = ({ children, activeMenu }) => {
+    const [screensize, setScreensize] = useState(null);
+    const handleResize = () => {
+        setScreensize(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
+    }, [])
     return (
         <div className="d-flex flex-column vh-100">
             <MainHeader menuActive="dashboard" panel={true}/>
             <div className="flex-grow-1 border-top">
                 <Container className="h-100">
                     <Row className="h-100">
-                        <Col lg={2} className="border-end p-0">
-                            <PanelSidebar activeMenu={activeMenu}/>
-                        </Col>
+                        {
+                            screensize > 991 ?
+                            <Col lg={2} className="border-end p-0">
+                                <PanelSidebar activeMenu={activeMenu}/>
+                            </Col>
+                            :
+                            <PanelSidebarMobile activeMenu={activeMenu}/>
+                        }
                         <Col lg={10} className="p-0">
-                            <div className="h-100 pt-5 pb-2 px-3 overflow-auto">
+                            <div className="h-100 pt-5 pb-2 px-md-3 overflow-auto" style={{ paddingLeft: screensize < 815 ? `60px` : "", paddingRight: screensize < 815 ? `12px` : "" }}>
                                 { children }
                             </div>
                         </Col>
