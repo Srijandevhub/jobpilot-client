e7a2030cfd698d96c6c4182d5d2f74f73d1c7869
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-04-30 15:46:16 +0530

    Update JobBox.jsx

--- src/components/JobBox/JobBox.jsx
+++ src/components/JobBox/JobBox.jsx
@@ -1,29 +1,28 @@
 import styles from './JobBox.module.css'
-import Google from '../../assets/google.svg'
 import Bookmark from '../../assets/bookmark.svg'
 import { Link } from 'react-router-dom'
 
-const JobBox = () => {
+const JobBox = ({ title, jobtype, minsal, maxsal, companyLogo, companyName, city, country, id }) => {
     return (
         <div className={styles.wrapper}>
-            <h5 className={styles.heading}>Techical Support Specialist</h5>
+            <h5 className={styles.heading}>{title}</h5>
             <div className='d-flex align-items-center mb-4'>
-                <span className={styles.tag}>Part Time</span>
-                <p className={styles.salary}>Salary: $20,000 - $25,000</p>
+                <span className={styles.tag}>{jobtype}</span>
+                <p className={styles.salary}>Salary: {minsal} - {maxsal}</p>
             </div>
             <div className={styles.media}>
                 <i className={styles.icon}>
-                    <img src={Google} alt='google'/>
+                    <img src={companyLogo} alt='google'/>
                 </i>
                 <div className={styles.content}>
-                    <p className={styles.name}>Google Inc.</p>
-                    <span className={styles.location}>Dhaka, Bangladesh</span>
+                    <p className={styles.name}>{companyName}</p>
+                    <span className={styles.location}>{city}, {country}</span>
                 </div>
                 <button className={styles.saveBtn}>
                     <img src={Bookmark} alt='bookmark'/>
                 </button>
             </div>
-            <Link to="/job-details" className={styles.blk}></Link>
+            <Link to={`/job-details/${id}`} className={styles.blk}></Link>
         </div>
     )
 }
