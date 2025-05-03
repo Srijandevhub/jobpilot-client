b12c55c2f36123ece22c3af54f1af4f03eff0912
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-03 16:48:08 +0530

    Update CompanyListing.jsx

--- src/components/CompanyListing/CompanyListing.jsx
+++ src/components/CompanyListing/CompanyListing.jsx
@@ -5,20 +5,31 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { baseUrl } from '../../url'
 import CompanyBox from '../CompanyBox/CompanyBox'
+import { useSearchParams } from 'react-router-dom'
 
 const CompanyListing = () => {
     const [query, setQuery] = useState("");
     const [companies, setCompanies] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
     useEffect(() => {
+        const queryParam = searchParams.get("q");
         const fetchCompanies = async () => {
             try {
-                const res = await axios.get(`${baseUrl}/api/v1/public/companies`);
+                const res = await axios.get(`${baseUrl}/api/v1/public/companies`, {
+                    params: {
+                        query: queryParam
+                    }
+                });
                 setCompanies(res.data.companies);
             } catch (error) {
                 console.log(error);
             }
         }
         fetchCompanies();
+    }, [searchParams])
+    useEffect(() => {
+        const queryParam = searchParams.get("q") || "";
+        setQuery(queryParam);
     }, [])
     return (
         <div className={styles.wrapper}>
@@ -31,7 +42,13 @@ const CompanyListing = () => {
                     <div className={styles.filter3}>
                         <div className={`d-flex align-items-center justify-content-end p-2 flex-wrap`}>
                             <button className={styles.btn} onClick={() => {
-                                
+                                const currentParams = Object.fromEntries([...searchParams]);
+                                if (query.length > 0) {
+                                    currentParams.q = query;
+                                } else {
+                                    delete currentParams.q;
+                                }
+                                setSearchParams(currentParams);
                             }}>Find Company</button>
                         </div>
                     </div>
