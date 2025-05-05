d679ffb37e919c181714f9e803cb665817319cf5
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-05 11:46:25 +0530

    Update AllJobs.jsx

--- src/components/Admin/AllJobs/AllJobs.jsx
+++ src/components/Admin/AllJobs/AllJobs.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { Table } from "react-bootstrap"
 import { Flip, toast } from "react-toastify";
 import { baseUrl } from "../../../url";
+import { Link } from "react-router-dom";
 
 const AllJobs = () => {
     const [jobs, setJobs] = useState([]);
@@ -55,7 +56,11 @@ const AllJobs = () => {
                                     <td>{job.title}</td>
                                     <td>{job.isarchived ? "Archived" : "Open"}</td>
                                     <td>
-                                        
+                                        <Link className="btn btn-primary" to={`/dashboard/applications/${job._id}`}>
+                                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7h1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h11.5M7 14h6m-6 3h6m0-10h.5m-.5 3h.5M7 7h3v3H7V7Z"/>
+                                            </svg>
+                                        </Link>
                                     </td>
                                 </tr>
                             )
