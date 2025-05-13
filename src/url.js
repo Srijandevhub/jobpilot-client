13e2f0a87817faff29f5078997917442772c7f57
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2025-05-13 16:00:33 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-//export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://jobpilot-server-726b.onrender.com";
+export const baseUrl = "http://localhost:5000";
+//export const baseUrl = "https://jobpilot-server-726b.onrender.com";
